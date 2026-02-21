fc41ed23353af814bb0a4bb4e1b489da69c493f3
Mac <mac@MacBook-Pro-de-Mac.local>
2026-02-21 11:15:22 -0300

    fix: ajuste base vite para produção

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production"
+    ? "/site_oficial/"   // nome EXATO do repositório no GitHub
+    : "/",
+
   plugins: [react()],
-  base: "/pro-excellence-hub/"
-});
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
